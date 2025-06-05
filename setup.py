9fe4b87bf2bbf981618d51949ad16e708742318e
Ishika <ishikajaiswal213@gmail.com>
2025-06-05 19:13:50 +1000

    changes in setup

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/tree_navigation_launch.py']),
-        ('share/' + package_name, ['config/tree_params.yaml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
