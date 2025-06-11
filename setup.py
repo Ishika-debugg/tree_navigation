c3eda96d63388116462a97343ce6c8cb331b260e
Ishika <ishikajaiswal213@gmail.com>
2025-06-11 17:14:45 +1000

    changes in the setup

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'tree_navigation'
 
@@ -9,7 +11,12 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        # ADD THESE LINES ⬇️
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
